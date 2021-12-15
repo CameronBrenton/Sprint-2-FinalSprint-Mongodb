8b74f13951c765382be187f484b3d54752da9e7d
CameronBrenton <Cameron.Brenton@keyin.com>
2021-12-15 18:29:12 -0330

    Change controller port from 80801 to 9091

--- src/main/java/com/example/sprintmongo/controller/AnimalController.java
+++ src/main/java/com/example/sprintmongo/controller/AnimalController.java
@@ -11,7 +11,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
-@CrossOrigin(origins = "http://localhost:8081")
+@CrossOrigin(origins = "http://localhost:9091")
 @RestController
 @RequestMapping("/api")
 public class AnimalController {
