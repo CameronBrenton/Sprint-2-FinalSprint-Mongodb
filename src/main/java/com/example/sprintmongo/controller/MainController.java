9e7cbf3729eb31141bec28ce551ad3a632ae3a43
CameronBrenton <Cameron.Brenton@keyin.com>
2021-12-19 16:05:01 -0330

    Remove ModelAndView import

--- src/main/java/com/example/sprintmongo/controller/MainController.java
+++ src/main/java/com/example/sprintmongo/controller/MainController.java
@@ -14,7 +14,6 @@ import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.servlet.ModelAndView;
 
 import java.util.ArrayList;
 import java.util.List;
