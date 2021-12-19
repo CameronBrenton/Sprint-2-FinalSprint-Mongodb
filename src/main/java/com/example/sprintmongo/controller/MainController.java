527e03d2022e88d9496e6612c147dc88fab5e492
CameronBrenton <Cameron.Brenton@keyin.com>
2021-12-19 16:07:07 -0330

    Remove import

--- src/main/java/com/example/sprintmongo/controller/MainController.java
+++ src/main/java/com/example/sprintmongo/controller/MainController.java
@@ -14,6 +14,7 @@ import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 import java.util.ArrayList;
 import java.util.List;
